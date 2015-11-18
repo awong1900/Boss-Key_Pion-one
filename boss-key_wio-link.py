a1db1484ebf2699da5cd76ae637d3ee009413704
Ten Wong <wangtengoo7@gmail.com>
2015-11-18 23:59:21 +0800

    prove

--- boss-key_wio-link.py
+++ boss-key_wio-link.py
@@ -1,6 +1,7 @@
 import os
 from websocket import create_connection
 import requests
+import time
 
 wio_link_server = "wss://cn.iot.seeed.cc/v1/node/event"
 wio_link_key = "efe19ae9752add26d614d87cacd97f45"
@@ -16,7 +17,10 @@ while True:
     print "Some guys is coming..."
     os.system("open /Applications/Mail.app")
 
-    requests.post("https://cn.iot.seeed.cc/v1/node/GroveServo/angle/10?access_token=efe19ae9752add26d614d87cacd97f45")
+    requests.post("https://cn.iot.seeed.cc/v1/node/GroveServo/angle/180?access_token=efe19ae9752add26d614d87cacd97f45")
     requests.post("https://cn.iot.seeed.cc/v1/node/GroveLedWs2812/clear/40/800000?access_token=efe19ae9752add26d614d87cacd97f45")
 
+    time.sleep(1)
+    requests.post("https://cn.iot.seeed.cc/v1/node/GroveServo/angle/90?access_token=efe19ae9752add26d614d87cacd97f45")
+    requests.post("https://cn.iot.seeed.cc/v1/node/GroveLedWs2812/clear/40/008000?access_token=efe19ae9752add26d614d87cacd97f45")
 ws.close()
