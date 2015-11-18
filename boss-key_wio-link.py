b8f1cd9ad13852648433cc107fe17e63ba567441
Ten Wong <wangtengoo7@gmail.com>
2015-11-18 23:24:30 +0800

    Added PIR sensor

---
+++ boss-key_wio-link.py
@@ -0,0 +1,22 @@
+import os
+from websocket import create_connection
+import requests
+
+wio_link_server = "wss://cn.iot.seeed.cc/v1/node/event"
+wio_link_key = "efe19ae9752add26d614d87cacd97f45"
+
+ws = create_connection(wio_link_server)
+ws.send(wio_link_key)
+print "link to pion one sensor."
+
+while True:
+    print "Receiving..."
+    result = ws.recv()
+    print "Received '%s'" % result
+    print "Some guys is coming..."
+    os.system("open /Applications/Mail.app")
+
+    requests.post("https://cn.iot.seeed.cc/v1/node/GroveServo/angle/10?access_token=efe19ae9752add26d614d87cacd97f45")
+    requests.post("https://cn.iot.seeed.cc/v1/node/GroveLedWs2812/clear/40/800000?access_token=efe19ae9752add26d614d87cacd97f45")
+
+ws.close()
